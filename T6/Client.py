8934f3185300cecc516992e6506d51ba38c37071
HacksDev <mocrousov.and@yandex.ru>
2020-02-02 14:23:20 +0300

    encoding fix

--- T6/Client.py
+++ T6/Client.py
@@ -7,26 +7,36 @@ class Client:
     socket = None
     threadRead = None
     threadWrite = None
+    threadRunningStatus = None
+    nickname = None
 
     def terminate(self):
-        print("[INFO] Connection was closed!")
+        self.threadRunningStatus = False
+        self.socket.close()
+        print("[INFO] Connection was closed! Goodbye!")
 
     def recv_data(self):
-        while True:
-            try:
-                data = self.socket.recv(4096)
-            except:
-                self.terminate()
-                break
-            if data:
-                print("Received data: ", data)
+        while self.threadRunningStatus:
+            while True:
+                try:
+                    data = self.socket.recv(4096)
+                except:
+                    break
+                if data:
+                    print("Received data: ", str(data, encoding="UTF-8"))
+                else:
+                    self.terminate()
 
     def send_data(self):
-        request = input()
-        while request != ".quit":
-            self.socket.send(bytes(request, encoding="UTF-8"))
-            request = input()
-        self.socket.close()
+        while self.threadRunningStatus:
+            try:
+                request = input()
+                while request != ".quit":
+                    self.socket.send(bytes(request, encoding="UTF-8"))
+                    request = input()
+                self.terminate()
+            except Exception:
+                pass
 
     def __init__(self):
         try:
@@ -35,15 +45,19 @@ class Client:
         except Exception:
             print("[ERROR] Server is not available now!")
             exit(-1)
-        print("[INFO] Welcome in our simple TCP chat.\n Type !'.quit'! if you want to leave!")
+        while self.nickname == None or self.nickname == "":
+            self.nickname = input("[INFO] Please type your nickname:")
+        self.socket.send(bytes("NICK:"+self.nickname, encoding="UTF-8"))
+        print("[INFO] Welcome in our simple TCP chat.\n Type '.quit' if you want to leave!")
         try:
             self.threadRead = threading.Thread(target=self.recv_data)
             self.threadWrite = threading.Thread(target=self.send_data)
+            self.threadRunningStatus = True
             self.threadRead.start()
             self.threadWrite.start()
         except Exception:
             self.socket.close()
-            print("[INFO] Connection was closed!")
+
 
 
 if __name__ == '__main__':
--- T6/Server.py
+++ T6/Server.py
@@ -8,12 +8,16 @@ class Server:
 
     inStream = list()
     outStream = list()
+    clientsInfo = list()
 
     def __init__(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setblocking(False)
-        self.socket.bind(self.SERVER_ADDR)
-        self.socket.listen(self.MAX_DESC)
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.setblocking(False)
+            self.socket.bind(self.SERVER_ADDR)
+            self.socket.listen(self.MAX_DESC)
+        except Exception:
+            print("[ERROR] Cannot to start. Probably you need to be root-user for launch the app!")
 
     def run(self):
         self.inStream.append(self.socket)
@@ -24,7 +28,7 @@ class Server:
                 self.on_event(readables)
         except KeyboardInterrupt:
             self.free_res(self.socket)
-            print("[INFO] Server stopped!")
+            print("[INFO] Server was stopped!")
 
     def free_res(self, resource):
         if resource in self.outStream:
@@ -32,35 +36,48 @@ class Server:
         if resource in self.inStream:
             self.inStream.remove(resource)
         resource.close()
-        print('closing connection ' + str(resource))
+        print('[INFO] Closing connection ' + str(resource))
 
-    def on_event(self, readables):
-        for resource in readables:
-            if resource is self.socket:
-                connection, client_address = resource.accept()
-                connection.setblocking(0)
-                self.inStream.append(connection)
-                print("new connection from {address}".format(address=client_address))
+    def accept_new_client(self, resource):
+        connection, client_address = resource.accept()
+        connection.setblocking(0)
+        self.inStream.append(connection)
+        print("New client {address}".format(address=client_address))
+
+    def service_action(self, resource, data):
+        pass
+
+    def receive_data(self, resource):
+        data = ""
+        try:
+            data = resource.recv(1024)
+        except ConnectionResetError:
+            print("[WARNING] Connection was closed!")
+        if data:
+            data = str(data, encoding="UTF-8")
+            print("[TEXT-LOG]: {}".format(data))
+            if resource not in self.outStream:
+                self.outStream.append(resource)
+            if data.find("#[SERVICE]#") != -1:
+                self.service_action(resource, data)
             else:
-                data = ""
-                try:
-                    data = resource.recv(1024)
-                except ConnectionResetError:
-                    pass
-                if data:
-                    print("getting data: {}".format(str(data)))
-                    if resource not in self.outStream:
-                        self.outStream.append(resource)
-                    self.send_all(str(data, encoding="UTF-8"))
-                else:
-                    self.free_res(resource)
+                self.send_answer(data)
+        else:
+            self.free_res(resource)
 
-    def send_all(self, message):
+    def send_answer(self, message):
         for resource in self.outStream:
             try:
                 resource.send(bytes(message, encoding='UTF-8'))
             except OSError:
                 self.free_res(resource)
+
+    def on_event(self, readables):
+        for resource in readables:
+            if resource is self.socket:
+                self.accept_new_client(resource)
+            else:
+                self.receive_data(resource)
 
 if __name__ == '__main__':
     server = Server()
