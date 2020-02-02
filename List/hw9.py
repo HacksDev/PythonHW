5d41e75124a192fb2c9bc2bd1f47dd049595944a
HacksDev <mocrousov.and@yandex.ru>
2020-02-02 14:29:56 +0300

    fix hw9.py

--- List/hw9.py
+++ List/hw9.py
@@ -2,7 +2,7 @@
 print((sum([x for x in range(1,101)]))**2 - sum([x**2 for x in range(1,101)])) # 25164150
 
 ## Report 9
-print([(x*y*(x**2 + y**2)**(0.5)) for x in range(400) for y in range(300) if (x+y+(x**2+y**2)**0.5 == 1000 and x != 0 and y != 0)]) # 31875000
+print([(x*y*(x**2 + y**2)**(0.5)) for x in range(400) for y in range(300) if (x+y+(x**2+y**2)**0.5 == 1000)]) # 31875000
 
 ## Report 48
 print(sum([x**x for x in range(1,1001)]) % 10000000000) # 9110846700
